c2484b43f6504c2e528d2813a21eae920dc4a725
Munir Uddin <munirchittagong@yahoo.com>
2019-04-15 20:20:50 +0100

    42. Refactoring error tests

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -1,28 +1,7 @@
 require 'rails_helper'
 
 describe AccessTokensController do
-  describe '#create' do
-    shared_examples_for "unauthorized_requests" do
-      let(:authentication_error) do
-        {
-            "status" => "401",
-            "source" => { "pointer" => "/code" },
-            "title" => "Authentication code is invalid",
-            "detail" => "You must provide valid code in order to exchange it for token."
-        }
-      end
-
-      it 'should return 401 status code' do
-        subject
-        expect(response).to have_http_status(401)
-      end
-
-      it 'should return proper error body' do
-        subject
-        expect(json['errors']).to include(authentication_error)
-      end
-    end
-
+  describe 'POST #create' do
     context 'when no code provided' do
       subject { post :create }
       it_behaves_like "unauthorized_requests"
@@ -80,27 +59,16 @@ describe AccessTokensController do
   end
 
   describe 'DELETE #destroy' do
-    context 'when invalid request' do
-      let(:authorization_error) do
-        {
-            "status" => "403",
-            "source" => { "pointer" => "/headers/authorization" },
-            "title" => "Not authorized",
-            "detail" => "You have no right to access this resource."
-        }
-      end
+    subject { delete :destroy }
 
-      subject { delete :destroy }
+    context 'when no authorization header provided' do
+      it_behaves_like 'forbidden_requests'
+    end
 
-      it 'should return 403 status code' do
-        subject
-        expect(response).to have_http_status(:forbidden)
-      end
+    context 'when invalid authorization header provided' do
+      before { request.headers['authorization'] = 'Invalid token' }
 
-      it 'should return proper error json' do
-        subject
-        expect(json['errors']).to include(authorization_error)
-      end
+      it_behaves_like 'forbidden_requests'
     end
 
     context 'when valid request' do
