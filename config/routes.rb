1aa64f5df1a107e9d4bedad78e7850d4bf822aab
Munir Uddin <munirchittagong@yahoo.com>
2019-04-19 16:35:26 +0100

    49. Article removal showcase

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :articles, only: [:index, :show, :create, :update]
+  resources :articles
 
   post '/login', to: 'access_tokens#create'
   delete '/logout', to: 'access_tokens#destroy'
