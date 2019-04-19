c0ff31dd5c42994f4b9f664d36c52c741b7b69e9
Munir Uddin <munirchittagong@yahoo.com>
2019-04-19 17:02:50 +0100

    50. Listing comments - scaffolds cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :articles
+  resources :articles do
+    resources :comments, only: [:index, :create]
+  end
 
   post '/login', to: 'access_tokens#create'
   delete '/logout', to: 'access_tokens#destroy'
